0a6292ca99c1db10ded6a852cb87c2ff3bdac868
ottokang <ottokang@gmail.com>
2021-12-07 17:27:23 +0800

    新增測試倒數計時按鈕

--- js/dev.js
+++ js/dev.js
@@ -3,12 +3,17 @@
 var isDevelopement = 0;
 
 if (isDevelopement === 1) {
-    // 顯示訊息
+    // 顯示測試用訊息
     $("#recorder_time").html("錄影時間測試").show();
     $("#countdown_time").html("倒數時間").show();
     $("#message").html("訊息測試").show();
     $("#recorder_time").html("錄影時間測試").show();
     $("#file_size").html("檔案大小").show();
+
+    // 綁定測試函數
+    $("#countdown_test").on("click", function() {
+        recorderCountdown(3);
+    });
 
     // 顯示測試選項
     $(".dev").show();
